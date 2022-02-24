21f286428b6fd9d7836d1cbf3932605ba3ff1c64
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-02-24 12:29:49 +0530

    Add files via upload

---
+++ Star Patterns/Star Pattern 1.java
@@ -0,0 +1,13 @@
+ class Star {
+     public static void main(String[] args) {
+     System.out.println("*");
+     System.out.println("**");
+     System.out.println("***");
+     System.out.println("****");
+     System.out.println("*****");
+
+     //Or Another Way
+
+     System.out.print(" *\n **\n ***\n ****\n *****\n");
+    }
+}
---
+++ Star Patterns/Z pattern.java
@@ -0,0 +1,10 @@
+class test {
+     public static void main(String[] args) {
+     System.out.println("*****");
+     System.out.println("   *");
+     System.out.println("  *");
+     System.out.println(" *");
+     System.out.println("*****");
+    
+    }
+}
