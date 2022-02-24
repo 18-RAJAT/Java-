6bf77c009593eeec8d8ccccb0d4120ba46a837b1
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-02-24 13:47:48 +0530

    Add files via upload

---
+++ Conditional Statement/Statements.java
@@ -0,0 +1,43 @@
+class Satement {
+     public static void main(String[] args) {
+       
+        int x = 10;
+        
+       if(x % 2 == 0)
+       {
+           System.out.println("even\n");
+       }
+
+       else
+       {
+           System.out.println("odd\n");
+       }
+
+
+     //Code Smaller and Greater Program
+     int n1 = 10;
+     int n2 = 20;
+     
+     if(n1 == n2)
+     {
+         System.out.println("Both are equal");
+     }
+
+     else
+     {
+         if(n1>n2)
+         {
+             System.out.println("n1 is greater then n2");
+         }
+
+         else
+         {
+             System.out.println("n2 is greater then n1");
+         }
+     }
+
+     System.out.println("Successfully run");
+
+
+    }
+}
