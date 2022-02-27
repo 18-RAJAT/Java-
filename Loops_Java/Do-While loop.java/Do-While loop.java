076053ef451ef920a7d75a662bdc57d83cf7d28f
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-02-27 12:50:26 +0530

    Add files via upload

---
+++ Loops_Java/Do-While loop.java/Do-While loop.java
@@ -0,0 +1,11 @@
+class test {
+     public static void main(String[] args) {
+     int i=0;    
+     System.out.println("Do-While loop");
+   
+    do{    
+        System.out.println(i);    
+    i++;    
+    }while(i<=10);    
+}
+}
