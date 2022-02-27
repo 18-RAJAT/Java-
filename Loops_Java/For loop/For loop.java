23baf4b9a7742674f931ed142851f220f87b3f63
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-02-27 12:46:28 +0530

    Add files via upload

---
+++ Loops_Java/For loop/For loop.java
@@ -0,0 +1,12 @@
+class test {
+     public static void main(String[] args) {
+     System.out.println("For loop");
+
+     for(int i=0;i<=9;++i)
+     {
+         System.err.println(i);
+     }
+
+     System.out.println("End");
+    }
+}
