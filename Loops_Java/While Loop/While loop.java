74f7b7981e1510165277e3841f038c9042dddb70
Rajat_Joshi <78920780+18-RAJAT@users.noreply.github.com>
2022-02-27 12:42:30 +0530

    Add files via upload

---
+++ Loops_Java/While Loop/While loop.java
@@ -0,0 +1,16 @@
+class test {
+     public static void main(String[] args) {
+         int i=0;
+         while(i<=9)
+    {
+     System.out.println(i);
+     i = i+1;
+
+     //or
+
+     i++;
+    }
+
+    System.out.println("END");
+   }
+} 
